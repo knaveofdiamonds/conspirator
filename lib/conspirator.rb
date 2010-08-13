51276aec1d6907dca4c81a3a8a8ff594e0e3b3b7
Roland Swingler <roland.swingler@gmail.com>
2010-08-13 20:15:43 +0100

    Moving specs and other file renames.

--- lib/conspirator.rb
+++ lib/conspirator.rb
@@ -1,2 +1,2 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "conspirator", "scaled_array"))
-require File.expand_path(File.join(File.dirname(__FILE__), "conspirator", "data_encoder"))
+require File.expand_path(File.join(File.dirname(__FILE__), "conspirator", "data_encoding"))
--- spec/data_encoding_spec.rb
+++ spec/data_encoding_spec.rb
@@ -33,33 +33,3 @@ describe "Google Charts' extended data encoding" do
   end
 end
 
-describe "Data Encodingter (using the ExtendedEncoding)" do
-  it "should return 1 if all values are between 0 and 4095" do
-    ScaledArray.new([0, 4095, 666], 4095).divisor.should == 1
-  end
-
-  it "should return a scaling factor if a value is bigger than 4095" do
-    ScaledArray.new([0, 8190, 666], 4095).divisor.should == 2
-  end
-
-  it "should return a scaling factor of 1 if the spread is 4095" do
-    ScaledArray.new([4095, 8190, 5666], 4095).divisor.should == 1
-  end
-
-  it "should return a scaling factor of 2 if the spread is 8190" do
-    ScaledArray.new([-4095, 4095, 666], 4095).divisor.should == 2
-  end
-  
-  it "should return offset amount so that the lowest value will be 0" do
-    ScaledArray.new([-4095, 4095, 666], 4095).offset.should == 4095
-    ScaledArray.new([4095, 8190, 5666], 4095).offset.should == -4095
-  end
-  
-  it "should scale every value in the array with fractional values rounded" do
-    ScaledArray.new([-4095, 4095, 666], 4095).scaled_data.should == [0, 4095, (2048 + 333)]
-  end
-
-  it "should delegate methods to array" do
-    ScaledArray.new([-4095, 4095, 666], 4095).first.should == 0
-  end
-end
